539da8813ef374fc0ca3d14b044ee177b935b419
Johannes-Herz <herz.johannes@gmx.net>
2023-02-21 19:53:13 +0100

    Removed test data from constructor.

--- application/src/app/base/table/table.component.ts
+++ application/src/app/base/table/table.component.ts
@@ -11,17 +11,7 @@ export class TableComponent {
   @Input() hiddenColumns: Map<any, boolean> = new Map<any, boolean>; 
   @Input() clickListener: Function | null = null; 
 
-  constructor() {
-    this.thead = ["Feature1", "Feature2", "Feature3", "Feature4"];
-    this.tbody = [[34.5, 24.3, 65.4, 23],
-                  [1049, 3924, 3214.4, 2045],
-                  [1.3, 4.4, 3.7, 2.6],
-                  [-1, -3, -2.5, -10]];
-    this.hiddenColumns.set("Feature1", false);
-    this.hiddenColumns.set("Feature2", false);
-    this.hiddenColumns.set("Feature3", false);
-    this.hiddenColumns.set("Feature4", false);
-  }
+  constructor() {}
 
   onClick(){
     if(this.clickListener){
